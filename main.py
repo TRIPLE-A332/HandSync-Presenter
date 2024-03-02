6214d71d99c90537dc429000e62d2e2c84a98d88
Ali Ahmad Abdullah <abdullah.ahmad16j@gmail.com>
2024-03-02 11:26:14 +0530

    commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from cvzone.HandTrackingModule import HandDetector
+import cv2
+import os
+import numpy as np
+
+# Parameters
+width, height = 1280, 720
+gestureThreshold = 300
+folderPath = "Presentation"
+
+# Camera Setup
+cap = cv2.VideoCapture(0)
+cap.set(3, width)
+cap.set(4, height)
+
+# Hand Detector
+detectorHand = HandDetector(detectionCon=0.8, maxHands=1)
