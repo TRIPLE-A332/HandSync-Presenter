dce1fc9f2c2778882bc31b4a686fe18b85d718c3
zone1AA <100586875+zone1AA@users.noreply.github.com>
2024-03-28 16:48:43 +0530

    initial

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
-from cvzone.HandTrackingModule import HandDetector
 import cv2
 import os
-import numpy as np
+from cvzone.HandTrackingModule import HandDetector
 
-# Parameters
+#variables
 width, height = 1280, 720
-gestureThreshold = 300
-folderPath = "Presentation"
+folderPath =  ("Presentation")
 
-# Camera Setup
+#camera setup
 cap = cv2.VideoCapture(0)
-cap.set(3, width)
-cap.set(4, height)
+cap.set(3,width)
+cap.set(4,height)
 
-# Hand Detector
-detectorHand = HandDetector(detectionCon=0.8, maxHands=1)
-# Variables
-imgList = []
-delay = 30
-buttonPressed = False
-counter = 0
-drawMode = False
+#Getting Images of Presentation
+pathImages = sorted(os.listdir(folderPath), key= len)
+# print(pathImages)
+
+#variables
 imgNumber = 0
-delayCounter = 0
-annotations = [[]]
-annotationNumber = -1
-annotationStart = False
-hs, ws = int(120 * 1), int(213 * 1)  # width and height of small image
+hs, ws = int(120*2), int(213*2)
+gestureThreshold = 300
 
-# Get list of presentation images
-pathImages = sorted(os.listdir(folderPath), key=len)
-print(pathImages)
+#hand Detector
+detector = HandDetector(detectionCon=0.8, maxHands= 1)
+
+
+
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    pathFullImage = os.path.join(folderPath, pathImages[0])
+    imgCurrent = cv2.imread(pathFullImage)
+
+    hands, img = detector.findHands(img, flipType= False)
+    cv2.line(img, (0, gestureThreshold),(width , gestureThreshold),(0,25,0),10)
+
+    if hands:
+        hand = hands[0]
+        fingers = detector.fingersUp(hand)
+        print(fingers)
+
+
+    #Adding Webcam img on Slide
+    imgSmall = cv2.resize(img, (ws,hs))
+    h, w, _ = imgCurrent.shape
+    imgCurrent[0:hs,w-ws:w] = imgSmall
+
+
+    cv2.imshow("Image", img)
+    cv2.imshow("Slide", imgCurrent)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
